6ebc1d8b848ac8aae716615f5d4742a6e3c829cf
RobbieSo <147087669+RobbieSo@users.noreply.github.com>
2025-04-02 02:53:35 -0600

    Update CorsConfig.java

--- src/main/java/com/example/GymAdmin/config/CorsConfig.java
+++ src/main/java/com/example/GymAdmin/config/CorsConfig.java
@@ -1,4 +1,4 @@
-ppackage com.example.GymAdmin.config;
+package com.example.GymAdmin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
