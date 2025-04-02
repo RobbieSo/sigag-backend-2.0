7aa5e7461b3cc00e9d5482f4ee0de5a11a1d13ba
RobbieSo <rmiguelfg89@gmail.com>
2025-04-01 19:42:31 -0600

    remove coors

--- src/main/java/com/example/GymAdmin/config/CorsConfig.java
+++ src/main/java/com/example/GymAdmin/config/CorsConfig.java
@@ -10,12 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-                "http://localhost:5173",
-                "http://31.220.51.252:8080",
-                "http://aresgym.site",
-                "https://aresgym.site"
-            )
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
