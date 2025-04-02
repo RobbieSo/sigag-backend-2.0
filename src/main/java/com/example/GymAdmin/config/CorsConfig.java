161b82842fafc10cd96c7f85161e75475c57de58
RobbieSo <147087669+RobbieSo@users.noreply.github.com>
2025-04-02 02:50:35 -0600

    Update CorsConfig.java
    Cors http://31.220.51.252:8080

--- src/main/java/com/example/GymAdmin/config/CorsConfig.java
+++ src/main/java/com/example/GymAdmin/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.GymAdmin.config;
+ppackage com.example.GymAdmin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://31.220.51.252", "https://*.ngrok.io") // Aquí puedes añadir otros subdominios si usas ngrok
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
